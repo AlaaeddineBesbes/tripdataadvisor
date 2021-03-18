88ffccc6908a067ec3eb2a9a770e735a6e59d1e6
AlaaeddineBesbes <72502752+AlaaeddineBesbes@users.noreply.github.com>
2021-03-18 11:18:44 +0100

    Alaaeddine

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 
 def search_for_R(R_name):
-    html = urlopen("https://www.tripadvisor.com/Search?q="+R_name)
+    html = urlopen("https://www.tripadvisor.com/Restaurant_Review-g609074-d4064823-Reviews-Pizzahut-Coatbridge_North_Lanarkshire_Scotland.html")
     html_soup = BeautifulSoup(html, 'html.parser')
-    link = html_soup.find_all(class_="rebrand_2017 js_logging desktop_web Search")
+    link = html_soup.find_all(class_="review-container")
     print(link)
     
 search_for_R("majid")
